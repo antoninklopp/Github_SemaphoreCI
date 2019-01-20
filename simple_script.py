b3103a9f9603db1d04644ffe6bd8bd3daab32b91
Antonin <antonin.klopp-tosser@grenoble-inp.org>
2019-01-20 20:45:36 +0100

    Changed commit order

--- simple_script.py
+++ simple_script.py
@@ -4,8 +4,6 @@ import sys
 g = Github(sys.argv[1])
 
 repo = g.get_user().get_repo("Github_SemaphoreCI")
-print(repo)
 commits = repo.get_commits()
-c = commits[commits.totalCount - 1]
-print(c.committer)
+c = commits[0]
 c.create_comment("Automatic comment")
